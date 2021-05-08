2dffe5abd990741d0f1d6649a2dc5c5799346532
Andreyaschenko Mihail <m.andreyaschenko@corp.mail.ru>
2021-05-08 18:36:02 +0300

    Integrate boost

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include <cstdint>
 #include "types.hpp"
 #include "integrator.hpp"
-
+#include <boost/numeric/ublas/matrix.hpp>
 
 /* DELETE THIS FILE IN FUTURE AND USE ONLY FOR CUSTOM TESTS */
 
 int main() {
-    rndcmp::bfloat16sr a(-1.0f / 3.0f);
-    a = a - 4.0;
-    std::cout << a << std::endl;
+    double n = 5;
+    half_float::halfsr a(5);
+    rndcmp::Fixed<int32_t, 16> b(4);
+ 
+    // Eigen::Matrix<half_float::halfsr, 2, 2> m;
+    // m(0,0) = 4.5;
+    // m(1,0) = 4.5;
+    // m(0,1) = 4.5;
+    // m(1,1) = 4.5;
+    // std::cout << m << std::endl;
 }
